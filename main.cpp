c15fd89bb2373d57b3c6c78f6b16eb2e8facf719
Runer <runer53248@gmail.com>
2021-06-20 03:24:40 +0200

    Add Sort functions for Database. Add translateGender. Rename surename to surname. Removed Search* methods group from Student. Small formating fixes.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,21 @@
 int main() {
     Student student{"Jan", "Nowak", "Poznań", "smutna", "1", 123, "12345678901", Gender::Male};
     Student student2{"Anna", "Kowalska", "Katowice", "wesoła", "1", 4321, "98876543210", Gender::Female};
+    Student student3{"Anna", "Aleksandra", "Katowice", "wesoła", "1", 4321, "58876543210", Gender::Female};
 
     Database db;
     db.printAll();
     db.addStudent(student);
     db.addStudent(student2);
+    db.addStudent(student3);
+    db.printAll();
+
+    std::cout<<"---------SORT BY PESEL-----------\n";
+    db.sortByPesel();
+    db.printAll();
+
+    std::cout<<"---------SORT BY SURENAME-----------\n";
+    db.sortbySurename();
     db.printAll();
 
     return 0;
